37d9afb6ef008caaad8ccf3f253448759b4324b1
Wesley Rabachini Ribeiro <wesleyrr2015@gmail.com>
2022-05-22 18:08:54 -0300

    Comentando código

--- Challenge IOT/Screenshot.py
+++ Challenge IOT/Screenshot.py
@@ -3,14 +3,16 @@ import pyautogui
 import cv2
 import numpy
 
+# Tira print da tela e escreve o nome do arquivo
 foto = pyautogui.screenshot()
 nomeArquivo = datetime.datetime.now().strftime('%d-%m-%Y_%H%M%S' + '.png')
 
 
+# Salva Screen
 def Screen():
     foto.save('./img/' + nomeArquivo)
 
-
+# Mostra Screen
 def ShowScreenShot():
     imagem = numpy.array(foto)
     imagem = imagem[:, :, ::-1].copy()
--- Challenge IOT/TocarMusica.py
+++ Challenge IOT/TocarMusica.py
@@ -2,7 +2,6 @@ import os
 from random import randint
 import pygame
 from moviepy.editor import VideoFileClip
-
 from Speaker import Speaker
 
 
@@ -10,12 +9,15 @@ def TocarMusicaAleatoriaBaixada():
     arq = []
     music = []
 
+    #  Verifica os arquivos nas pastas
     for _, _, arquivo in os.walk('./video'):
         for name in arquivo:
             arq.append(name)
     for _, _, arquivo in os.walk('./music'):
         for name in arquivo:
             music.append(name)
+
+    # pega um arquivo que não é mp3 e transforma em mp3
     while True:
         video_name = arq[randint(0, len(arq) - 1)]
         if os.path.exists("./music/" + video_name[:-4] + ".mp3"):
@@ -26,8 +28,11 @@ def TocarMusicaAleatoriaBaixada():
             video = VideoFileClip(os.path.join("./video/" + video_name))
             video.audio.write_audiofile(os.path.join("./music/" + video_name[:-4] + ".mp3"))
             break
+
+    # inicia a música
     pygame.init()
     pygame.mixer.music.load("./music/" + video_name[:-4] + ".mp3")
     Speaker("Aperte Enter para parar a música")
     pygame.mixer.music.play()
     input("Press Enter:")
+    pygame.mixer.music.stop()
